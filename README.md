07beaa045f7c00224eb25823ffaca4c101aab773
Filip Allan Alberg <filipalberg99@gmail.com>
2025-09-01 15:09:10 +0200

    init api

--- README.md
+++
@@ -1,2 +0,0 @@
-# Recipe-book
-Recipe site where i can add my own recipes, for a central location for them. Will also contain a CMS for editing the content.
---
+++ RecipeAPI/DbContext/IDbContext.cs
@@ -0,0 +1,6 @@
+namespace RecipeAPI.DbContext;
+
+public interface IDbContext
+{
+    // Define the methods and properties that your DbContext should have
+}
